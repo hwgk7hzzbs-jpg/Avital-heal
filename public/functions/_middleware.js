24c3dd32a35c6a45b5cb7b4dc3f5ae952e782538
hwgk7hzzbs-jpg <hwgk7hzzbs@privaterelay.appleid.com>
2026-01-08 23:55:54 +0200

    Create _middleware.js

---
+++ public/functions/_middleware.js
@@ -0,0 +1,25 @@
+export async function onRequest(context) {
+  const response = await context.next();
+  
+  // הסר Headers ישנים שעלולים להתנגש
+  response.headers.delete('X-Frame-Options');
+  
+  // הוסף Security Headers
+  response.headers.set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload');
+  response.headers.set('X-Content-Type-Options', 'nosniff');
+  response.headers.set('Referrer-Policy', 'strict-origin-when-cross-origin');
+  response.headers.set('Permissions-Policy', 'geolocation=(), microphone=(), camera=()');
+  
+  // Content Security Policy
+  response.headers.set('Content-Security-Policy', 
+    "default-src 'self'; " +
+    "script-src 'self' 'unsafe-inline'; " +
+    "style-src 'self' 'unsafe-inline'; " +
+    "img-src 'self' data: https:; " +
+    "font-src 'self' data:; " +
+    "connect-src 'self'; " +
+    "frame-ancestors 'none';"
+  );
+  
+  return response;
+}
